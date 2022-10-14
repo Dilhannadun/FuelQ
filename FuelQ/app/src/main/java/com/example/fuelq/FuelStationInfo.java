6ddeebf7080c00cd15e2c9bd464f63c1f9f8f766
ChandulaM <chandula.madigasekara@gmail.com>
2022-10-14 19:02:42 +0530

    Added starting activities

---
+++ FuelQ/app/src/main/java/com/example/fuelq/FuelStationInfo.java
@@ -0,0 +1,14 @@
+package com.example.fuelq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuelStationInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_station_info);
+    }
+}
