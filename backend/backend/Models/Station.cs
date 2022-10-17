0724071de71d46e1db3b7b8b71eb3d6f5e58e192
ChandulaM <chandula.madigasekara@gmail.com>
2022-10-17 21:47:04 +0530

    Added models

---
+++ backend/backend/Models/Station.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Station
+    {
+        public string Id { get; set; }
+
+        public string StationName { get; set; }
+
+        public string City { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public float Petrol95Qty { get; set; }
+
+        public float Petrol92Qty { get; set; }
+
+        public float DieselQty { get; set; }
+
+        public Queue[] Queues { get; set; }
+    }
+}
